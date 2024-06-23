13ecc4e6811e0b6562165ba450833f2546035ca0
Siverslayer <164073129+Siverslayer@users.noreply.github.com>
2024-06-23 16:18:48 +0300

    Create py.py

---
+++ py.py
@@ -0,0 +1,35 @@
+# تحذير:
+#
+# إذا قمت بتنفيذ هذا الأمر على شخص ما، قد تواجه مشاكل قانونية أو أخلاقية خطيرة.
+# تأكد من الحصول على إذن واضح وصريح من الشخص المعني قبل المتابعة.
+# استخدام هذا النوع من البرامج بدون موافقة قد يؤدي إلى عواقب قانونية. 
+#1769 , 319
+#865 , 665 
+# 153 , 759
+# Warning:
+#
+# If you execute this command on someone, you may face serious legal or ethical issues.
+# Ensure you obtain clear and explicit permission from the person involved before proceeding.
+# Using this type of software without consent can result in legal consequences.
+
+
+
+
+
+import pyautogui as mouse
+
+x1=1769
+x2=865
+x3=153
+y1=319
+y2=665
+y3=759
+
+
+
+
+while True :
+    mouse.click(x1 ,y1)
+    mouse.click(x2 ,y2)
+    mouse.click(x3 ,y3)
+
